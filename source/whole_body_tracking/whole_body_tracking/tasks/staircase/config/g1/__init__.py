dcf9868758e18fd37078ec82ca02187a9998b1c4
kkarenvoo05 <kkarenvoo@gmail.com>
2026-03-06 15:36:10 -0800

    added task

--- source/whole_body_tracking/whole_body_tracking/tasks/staircase/config/g1/__init__.py
+++ source/whole_body_tracking/whole_body_tracking/tasks/staircase/config/g1/__init__.py
@@ -1,7 +1,7 @@
 import gymnasium as gym
 
 from . import agents
-from .staircase_env_cfg import G1StaircaseEnvCfg
+from .staircase_env_cfg import G1StaircaseEnvCfg, G1StaircasePlayEnvCfg
 from ...staircase_env import StaircaseEnv
 
 ##
@@ -24,6 +24,26 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": G1StaircaseEnvCfg,
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:G1StaircasePPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="Staircase-G1-Play-v0",
+    entry_point="whole_body_tracking.tasks.staircase.staircase_env:StaircaseEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": G1StaircasePlayEnvCfg,
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:G1StaircasePPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="Staircase-G1-Baseline-v0",
+    entry_point="whole_body_tracking.tasks.staircase.staircase_env:StaircaseEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": G1StaircasePlayEnvCfg,
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:G1StaircasePPORunnerCfg",
     },
 )
